deda1bd45ea53bec071023153cdbfbd71164222f
Giorgio <1999giorgio.monne@mail.com>
2022-02-10 00:59:32 -1000

    immesso i dati nel database col seeder

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Product;
 
 class ProductsTableSeeder extends Seeder
 {
@@ -11,6 +12,21 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $products = config("comics");
+
+        foreach($products as $product){
+
+            $newProduct = new Product();
+
+            $newProduct->title = $product["title"];
+            $newProduct->type = $product["type"];
+            $newProduct->series = $product["series"];
+            $newProduct->sale_date = $product["sale_date"];;
+            $newProduct->description = $product["description"];
+            $newProduct->price = $product["price"];
+            $newProduct->image = $product["thumb"];
+
+            $newProduct->save();
+        }
     }
 }
