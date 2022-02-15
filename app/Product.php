b9a74acc9856118287eab9ae2afef7158c3bf478
Giorgio <1999giorgio.monne@mail.com>
2022-02-15 09:39:28 -1000

    aggiustato piccolo errore nel product

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 
-    {protected $filable = ["title","type","series","sale_date","description","price","image"];
+    {protected $fillable = ["title","type","series","sale_date","description","price","image"];
 }
