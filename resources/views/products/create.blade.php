5e19e898f8c47f4db34cafc4b7492465e396551a
Giorgio <1999giorgio.monne@mail.com>
2022-02-15 03:58:55 -1000

    push aggiunte con errore caricamento 500

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -11,7 +11,7 @@
             @csrf
             <div class="form-group">
                 <label for="title">Titolo</label>
-                <input type="text" class="form-control @error("title") is-invalid @enderror" id="title"  name="title"  placeholder="Inserisci il fumetto" value="{{old("title")}}">
+                <input type="text" class="form-control " id="title"  name="title"  placeholder="Inserisci il fumetto" value="{{old("title")}}">
                 @error('title')
                     <div class="alert alert-danger">{{$message}}</div>
                 @enderror
@@ -19,9 +19,9 @@
 
             <div class="form-group">
                 <label for="type">Tipo fumetto</label>
-                <select class="form-control @error("title") is-invalid @enderror" id="type"  name="type"  placeholder="Tipologia fumetto" value="{{old("type")}}">
-                    <option value="book" {{old("type" == "book" ? "selected" : null)}}>Comic Book</option>
-                    <option value="novel" {{old("type" == "novel" ? "selected" : null)}}>Graphic Novel</option>
+                <select class="form-control @error("type") is-invalid @enderror" id="type"  name="type"  placeholder="Tipologia fumetto" value="{{old("type")}}">
+                    <option value="book" {{old("type") == "book" ? "selected" : null}}>Comic Book</option>
+                    <option value="novel" {{old("type") == "novel" ? "selected" : null}}>Graphic Novel</option>
                 </select>
                 @error('type')
                     <div class="alert alert-danger">{{$message}}</div>
@@ -30,7 +30,7 @@
 
             <div class="form-group">
                 <label for="series">Serie</label>
-                <input type="text" class="form-control @error("title") is-invalid @enderror" id="series"   name="series"  placeholder="Inserisci serie di appartenenza" value="{{old("series")}}">
+                <input type="text" class="form-control @error("series") is-invalid @enderror" id="series"   name="series"  placeholder="Inserisci serie di appartenenza" value="{{old("series")}}">
                 @error('series')
                     <div class="alert alert-danger">{{$message}}</div>
                 @enderror
@@ -38,7 +38,7 @@
 
             <div class="form-group">
                 <label for="sale_date">Data uscita</label>
-                <input type="date" class="form-control @error("title") is-invalid @enderror" id="sale_date"   name="sale_date"  placeholder="Inserisci la data d'uscita" value="{{old("sale_date")}}">
+                <input type="date" class="form-control @error("sale_date") is-invalid @enderror" id="sale_date"   name="sale_date"  placeholder="Inserisci la data d'uscita" value="{{old("sale_date")}}">
                 @error('sale_date')
                     <div class="alert alert-danger">{{$message}}</div>
                 @enderror
@@ -46,7 +46,7 @@
 
             <div class="form-group">
                 <label for="description">Descrizione</label>
-                <textarea class="form-control @error("title") is-invalid @enderror" name="description"  id="description" rows="8"  placeholder="Descrizione...">{{old("description")}}</textarea>
+                <textarea class="form-control @error("description") is-invalid @enderror" name="description"  id="description" rows="8"  placeholder="Descrizione...">{{old("description")}}</textarea>
                 @error('description')
                     <div class="alert alert-danger">{{$message}}</div>
                 @enderror
@@ -54,7 +54,7 @@
 
             <div class="form-group">
                 <label for="price">Prezzo</label>
-                <input type="number" class="form-control @error("title") is-invalid @enderror" id="price"   name="price"  placeholder="Digita il prezzo" value="{{old("price")}}">
+                <input type="number" class="form-control @error("price") is-invalid @enderror" id="price"   name="price"  placeholder="Digita il prezzo" value="{{old("price")}}">
                 @error('price')
                     <div class="alert alert-danger">{{$message}}</div>
                 @enderror
@@ -62,7 +62,7 @@
 
             <div class="form-group">
                 <label for="image">Immagine</label>
-                <input type="text" class="form-control @error("title") is-invalid @enderror" id="image"   name="image"  placeholder="Inserisci l'url dell'immagine" value="{{old("image")}}">
+                <input type="text" class="form-control @error("image") is-invalid @enderror" id="image"   name="image"  placeholder="Inserisci l'url dell'immagine" value="{{old("image")}}">
                 @error('image')
                     <div class="alert alert-danger">{{$message}}</div>
                 @enderror
